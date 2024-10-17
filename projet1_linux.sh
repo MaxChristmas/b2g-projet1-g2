4db5dfbfe1f040ea577be39c940f7785d92e89a7
FreddyCornil <fredc@mailo.com>
2024-10-17 21:38:55 +0200

    add color

--- projet1_linux.sh
+++ projet1_linux.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-DEBUG=1
 PORT="22"
+DEBUG=1
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+
 
 if [ $DEBUG -eq 1 ]; then
 	USER="wilder"
@@ -26,32 +32,32 @@ remoteCommand() {
 # Get the distant host name
 getHostName() {
 	REMOTE_NAME=$(remoteCommand "uname -n")
-	echo "Nom de machine distante: $REMOTE_NAME"
+	echo -e "${YELLOW}Nom de machine distante:${NC} ${RED}$REMOTE_NAME${NC}"
 }
 
 # Get the OS name and version
 getHostInfo() {
 	REMOTE_UNAME=$(remoteCommand "uname -sr")
-	echo "Nom de l'OS et version de la machine distante: $REMOTE_UNAME"
+	echo -e "${YELLOW}Nom de l'OS et version de la machine distante:${NC} ${RED}$REMOTE_UNAME${NC}"
 }
 
 # Get the user last connection date
 getUserLastConnection() {    
     REMOTE_DERNIERE_CO=$(remoteCommand "last $USER | head -n 1 | awk '{print \$1 \": depuis \" \$3 \" le \" \$4 \" \" \$5 \" \" \$6 \" \" \$7 \" \" \$8 \" \" \$9 \" \" \$10}'")
-    echo "Dernière connexion de $REMOTE_DERNIERE_CO"
+    echo -e "${YELLOW}Dernière connexion de${NC} ${RED}$REMOTE_DERNIERE_CO${NC}"
 }
 
 # Restart the distant device
 rebootRemoteDevice() {
 	REMOTE_REBOOT=$(remoteCommand "sudo reboot")
-	echo "Redémarrage de la machine distante"
+	echo -e "${YELLOW}Redémarrage de la machine distante${NC}"
 }
 
 # Retrieve distance device's disk state
 getDiskUsage() {
 	DISKUSAGE="df -h | grep 'sda' | awk '{print \$1 \" Espace Disponible: \" \$4 \"-\" \$5}'"
 	REMOTE_DISK=$(remoteCommand "$DISKUSAGE")
-	echo "Utilisation du disque: $REMOTE_DISK"
+	echo -e "${YELLOW}Utilisation du disque:${NC} ${RED}$REMOTE_DISK${NC}"
 }
 
 # Add a user
@@ -59,18 +65,25 @@ createUserAccount(){
 	read -p "Entrer le nom de l'utilisateur : " login_user	
 	REMOTE_ADD_USER=$(remoteCommand "sudo -s useradd $login_user -m")
 	REMOTE_ADD_USER=$(remoteCommand "sudo -s passwd $login_user")
+	echo -e "${YELLOW}L'utilisateur${NC} ${RED}$ogin_user${NC} ${YELLOW}a été ajoutté${NC}"
 }
 
 # Delete a user
 deleteUserAccount() {
 	read -p "Indiquer le nom d'utilisateur à supprimer: " user_deleted	
 	DEL_USER=$(remoteCommand "sudo userdel $user_deleted")
+	echo -e "${YELLOW}L'utilisateur${NC} ${RED}$user_deleted${NC} ${YELLOW}a été supprimé${NC}"
 }
 
 # Shutdown the distant device
 shutdownRemoteDevice() {
 	REMOTE_SHUTDOWN=$(remoteCommand "sudo shutdown now")
-	echo "Arrêt de la machine distante"
+	echo -e "${YELLOW}Arrêt de la machine distante${NC}"
+}
+
+# Exit command menu
+quit() {
+	echo -e "${YELLOW}Bye!\nAu plaisir de vous revoir!${NC}"
 }
 
 #Menu's choices listing
@@ -85,6 +98,10 @@ menu1_opt8="Etat du disque"
 menu1_opt9="Redémarrer"
 menu1_opt10="Arrêter"
 menu1_opt11="Quitter"
+
+echo -e "${GREEN}+++++++++++++++++++++++++++++++++++++++++++++++++++++++${NC}"
+echo -e "${GREEN}+++ Choisir une action ou afficher des informations +++${NC}"
+echo -e "${GREEN}+++++++++++++++++++++++++++++++++++++++++++++++++++++++${NC}"
 
 # Select and options prompt
 PS3="Sélectionner votre choix: "
@@ -123,8 +140,9 @@ do
 			shutdownRemoteDevice
 			;;
 		$menu1_opt11)
+			quit
 			break
 			;;
-		*) echo "Invalid option $REPLY";;
+		*) echo -e "${RED}Invalid option $REPLY${NC}";;
     esac
 done
