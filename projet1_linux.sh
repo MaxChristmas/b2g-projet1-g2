c4d76bf2dc1582c8c2e7172be39763cb668e1932
Gwen <gwen.jeorge@gmail.com>
2024-10-17 16:39:53 +0200

    code trad

--- projet1_linux.sh
+++ projet1_linux.sh
@@ -5,63 +5,64 @@ USER="wilder"
 HOST="172.16.10.30"
 PORT="22"
 
-# Execute command on distant device
-remote_command() {
+# Execute defined command on distant device
+remoteCommand() {
     local COMMAND=$1
     ssh -p $PORT $USER@$HOST "$COMMAND"
     exit
 }
 
-# Utilise la commande uname pour obtenir le nom de la machine
-nomClient() {
-	REMOTE_NAME=$(remote_command "uname -n")
+# Get the distant host name
+getHostName() {
+	REMOTE_NAME=$(remoteCommand "uname -n")
 	echo "Nom de machine distante: $REMOTE_NAME"
 }
 
-# Utilise la commande uname pour obtenir le nom et la version de l'OS
-nomVerClient() {
-	REMOTE_UNAME=$(remote_command "uname -sr")
+# Get the OS name and version
+getHostInfo() {
+	REMOTE_UNAME=$(remoteCommand "uname -sr")
 	echo "Nom de l'OS et version de la machine distante: $REMOTE_UNAME"
 }
 
-# Utilise la commande last pour obtenir la dernière connexion de l'utilisateur
-derniereCo() {    
-    REMOTE_DERNIERE_CO=$(remote_command "last $USER | head -n 1 | awk '{print \$1 \": depuis \" \$3 \" le \" \$4 \" \" \$5 \" \" \$6 \" \" \$7 \" \" \$8 \" \" \$9 \" \" \$10}'")
+# Get the user last connection date
+getUserLastConnection() {    
+    REMOTE_DERNIERE_CO=$(remoteCommand "last $USER | head -n 1 | awk '{print \$1 \": depuis \" \$3 \" le \" \$4 \" \" \$5 \" \" \$6 \" \" \$7 \" \" \$8 \" \" \$9 \" \" \$10}'")
     echo "Dernière connexion de $REMOTE_DERNIERE_CO"
 }
 
-# Redémarre la machine distante
-rebootRemote() {
-	REMOTE_REBOOT=$(remote_command "sudo reboot")
+# Restart the distant device
+rebootRemoteDevice() {
+	REMOTE_REBOOT=$(remoteCommand "sudo reboot")
 	echo "Redémarrage de la machine distante"
 }
 
-# Utilise la commande df pour récupérer les infos disque
+# Retrieve distance device's disk state
 getDiskUsage() {
 	DISKUSAGE="df -h | grep 'sda' | awk '{print \"Disque: \" \$1 \" Espace Disponible : \" \$4 \"-\" \$5}'"
-	REMOTE_DISK=$(remote_command "$DISKUSAGE")
+	REMOTE_DISK=$(remoteCommand "$DISKUSAGE")
 	echo "Utilisation du disque: $REMOTE_DISK"
 }
 
-# Ajout d'un utilisateur
+# Add a user
 createUserAccount(){
 	read -p "Entrer le nom de l'utilisateur : " login_user	
-	REMOTE_ADD_USER=$(remote_command "sudo -s useradd $login_user -m")
-	REMOTE_ADD_USER=$(remote_command "sudo -s passwd $login_user")
+	REMOTE_ADD_USER=$(remoteCommand "sudo -s useradd $login_user -m")
+	REMOTE_ADD_USER=$(remoteCommand "sudo -s passwd $login_user")
 }
 
-# Suppression d'utilisateur
+# Delete a user
 deleteUserAccount() {
 	read -p "Indiquer le nom d'utilisateur à supprimer: " user_deleted	
-	DEL_USER=$(remote_command "sudo userdel $user_deleted")
+	DEL_USER=$(remoteCommand "sudo userdel $user_deleted")
 }
 
-# Arrêt de la machine distante
-shutdownRemote() {
-	REMOTE_SHUTDOWN=$(remote_command "sudo shutdown now")
+# Shutdown the distant device
+shutdownRemoteDevice() {
+	REMOTE_SHUTDOWN=$(remoteCommand "sudo shutdown now")
 	echo "Arrêt de la machine distante"
 }
 
+#
 menu1_opt1="Création d'un compte utilisateur"
 menu1_opt2="Suppression d'un compte utilisateur"
 menu1_opt3="Ajout d'un utilisateur dans un groupe"
@@ -73,7 +74,7 @@ menu1_opt8="Redémarrer"
 menu1_opt9="Arrêter"
 menu1_opt10="Quitter"
 
-# Prompt du select et options
+# Select and options prompt
 PS3="Sélectionner votre choix: "
 options=("$menu1_opt1" "$menu1_opt2" "$menu1_opt3" "$menu1_opt4" "$menu1_opt5" "$menu1_opt6" "$menu1_opt7" "$menu1_opt8" "$menu1_opt9" "$menu1_opt10")
 select opt in "${options[@]}"
@@ -89,22 +90,22 @@ do
 			
 			;;
 		$menu1_opt4)
-			derniereCo
+			getUserLastConnection
 			;;
 		$menu1_opt5)
-			nomClient
+			getHostName
 			;;
 		$menu1_opt6)
-			nomVerClient
+			getHostInfo
 			;;
 		$menu1_opt7)
 			getDiskUsage
 			;;
 		$menu1_opt8)
-			rebootRemote
+			rebootRemoteDevice
 			;;
 		$menu1_opt9)
-			shutdownRemote
+			shutdownRemoteDevice
 			;;
 		$menu1_opt10)
 			break
