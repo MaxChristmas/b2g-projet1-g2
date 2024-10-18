0d49886f389a5ec76918d6b9595f771fd0782686
FreddyCornil <fredc@mailo.com>
2024-10-18 12:09:35 +0200

    logging added

--- projet1_linux.sh
+++ projet1_linux.sh
@@ -11,6 +11,21 @@ MAGENTA='\033[0;35m'
 CYAN='\033[0;36m'
 NC='\033[0m' # No Color
 
+#Menu's choices listing
+echo -e "${BLUE}+++++++++++++++++++++++++++++++++++++++++++++++++++++++${NC}"
+echo -e "${BLUE}    Choisir une action ou afficher des informations${NC}"
+echo -e "${BLUE}+++++++++++++++++++++++++++++++++++++++++++++++++++++++${NC}"
+menu1_opt1="Création d'un compte utilisateur"
+menu1_opt2="Suppression d'un compte utilisateur"
+menu1_opt3="Création d'un groupe"
+menu1_opt4="Ajout d'un utilisateur dans un groupe"
+menu1_opt5="Dernière connexion de l'utilisateur"
+menu1_opt6="Nom de la machine"
+menu1_opt7="Nom et Version de l'OS"
+menu1_opt8="Etat du disque"
+menu1_opt9="Redémarrer"
+menu1_opt10="Arrêter"
+menu1_opt11="Quitter"
 
 if [ $DEBUG -eq 1 ]; then
 	USER="wilder"
@@ -32,43 +47,13 @@ remoteCommand() {
     exit
 }
 
-# Get the distant host name
-getHostName() {
-	REMOTE_NAME=$(remoteCommand "uname -n")
-	echo -e "${YELLOW}Nom de machine distante:${NC} ${RED}$REMOTE_NAME${NC}"
-}
-
-# Get the OS name and version
-getHostInfo() {
-	REMOTE_UNAME=$(remoteCommand "uname -sr")
-	echo -e "${YELLOW}Nom de l'OS et version de la machine distante:${NC} ${RED}$REMOTE_UNAME${NC}"
-}
-
-# Get the user last connection date
-getUserLastConnection() {    
-    REMOTE_DERNIERE_CO=$(remoteCommand "last $USER | head -n 1 | awk '{print \$1 \": depuis \" \$3 \" le \" \$4 \" \" \$5 \" \" \$6 \" \" \$7 \" \" \$8 \" \" \$9 \" \" \$10}'")
-    echo -e "${YELLOW}Dernière connexion de${NC} ${RED}$REMOTE_DERNIERE_CO${NC}"
-}
-
-# Restart the distant device
-rebootRemoteDevice() {
-	REMOTE_REBOOT=$(remoteCommand "sudo reboot")
-	echo -e "${YELLOW}Redémarrage de la machine distante${NC}"
-}
-
-# Retrieve distance device's disk state
-getDiskUsage() {
-	DISKUSAGE="df -h | grep 'sda' | awk '{print \$1 \" Espace Disponible: \" \$4 \"-\" \$5}'"
-	REMOTE_DISK=$(remoteCommand "$DISKUSAGE")
-	echo -e "${YELLOW}Utilisation du disque:${NC} ${RED}$REMOTE_DISK${NC}"
-}
-
 # Add a user
 createUserAccount(){
 	read -p "Entrer le nom de l'utilisateur : " login_user	
 	REMOTE_ADD_USER=$(remoteCommand "sudo -s useradd $login_user -m")
 	REMOTE_ADD_USER=$(remoteCommand "sudo -s passwd $login_user")
 	echo -e "${YELLOW}L'utilisateur${NC} ${RED}$login_user${NC} ${YELLOW}a été ajoutté${NC}"
+	log $USER "$menu1_opt1 : L'utilisateur $login_user été ajoutté"
 }
 
 # Delete a user
@@ -76,19 +61,15 @@ deleteUserAccount() {
 	read -p "Indiquer le nom d'utilisateur à supprimer: " user_deleted	
 	DEL_USER=$(remoteCommand "sudo userdel -r $user_deleted")
 	echo -e "${YELLOW}L'utilisateur${NC} ${RED}$user_deleted${NC} ${YELLOW}a été supprimé${NC}"
-}
-
-# Shutdown the distant device
-shutdownRemoteDevice() {
-	REMOTE_SHUTDOWN=$(remoteCommand "sudo shutdown now")
-	echo -e "${YELLOW}Arrêt de la machine distante${NC}"
+	log $USER "$menu1_opt2 : L'utilisateur $user_deleted a été supprimé"
 }
 
 # Add group
 addGroup() {
 	read -p "Entrer le nom du groupe à créer: " newgroup	
 	REMOTE_SHUTDOWN=$(remoteCommand "sudo groupadd $newgroup")
-	echo -e "${YELLOW}Le groupe${NC} ${RED}$newgroup${NC} ${YELLOW}a été ajouté${NC}" 
+	echo -e "${YELLOW}Le groupe${NC} ${RED}$newgroup${NC} ${YELLOW}a été ajouté${NC}"
+	log $USER "$menu1_opt3 : Le groupe $newgroup a été ajouté"
 }
  
 # Add user in group
@@ -97,6 +78,50 @@ addUserInGroup() {
     read -p "Entrer le nom du groupe : " group
     REMOTE_SHUTDOWN=$(remoteCommand "sudo usermod -aG $group $userg")
     echo -e "${YELLOW}L'utilisateur${NC} ${RED}$userg${NC} ${YELLOW}a été ajouté dans${NC} ${RED}$group${NC}"
+    log $USER "$menu1_opt4 : L'utilisateur $userg a été ajouté dans $group"
+}
+
+# Get the user last connection date
+getUserLastConnection() {    
+    REMOTE_DERNIERE_CO=$(remoteCommand "last $USER | head -n 1 | awk '{print \$1 \": depuis \" \$3 \" le \" \$4 \" \" \$5 \" \" \$6 \" \" \$7 \" \" \$8 \" \" \$9 \" \" \$10}'")
+    echo -e "${YELLOW}Dernière connexion de${NC} ${RED}$REMOTE_DERNIERE_CO${NC}"
+    log $USER "$menu1_opt5 : $REMOTE_DERNIERE_CO"
+}
+
+# Get the distant host name
+getHostName() {
+	REMOTE_NAME=$(remoteCommand "uname -n")
+	echo -e "${YELLOW}Nom de machine distante:${NC} ${RED}$REMOTE_NAME${NC}"
+	log $USER "$menu1_opt6 : $REMOTE_NAME"
+}
+
+# Get the OS name and version
+getHostInfo() {
+	REMOTE_UNAME=$(remoteCommand "uname -sr")
+	echo -e "${YELLOW}Nom de l'OS et version de la machine distante:${NC} ${RED}$REMOTE_UNAME${NC}"
+	log $USER "$menu1_opt7 : $REMOTE_UNAME"
+}
+
+# Retrieve distance device's disk state
+getDiskUsage() {
+	DISKUSAGE="df -h | grep 'sda' | awk '{print \$1 \" Espace Disponible: \" \$4 \"-\" \$5}'"
+	REMOTE_DISK=$(remoteCommand "$DISKUSAGE")
+	echo -e "${YELLOW}Utilisation du disque:${NC} ${RED}$REMOTE_DISK${NC}"
+	log $USER "$menu1_opt8 $REMOTE_DISK"
+}
+
+# Restart the distant device
+rebootRemoteDevice() {
+	REMOTE_REBOOT=$(remoteCommand "sudo reboot")
+	echo -e "${YELLOW}Redémarrage de la machine distante${NC}"
+	log $USER "$menu1_opt9"
+}
+
+# Shutdown the distant device
+shutdownRemoteDevice() {
+	REMOTE_SHUTDOWN=$(remoteCommand "sudo shutdown now")
+	echo -e "${YELLOW}Arrêt de la machine distante${NC}"
+	log $USER "$menu1_opt10 : Arrêt de la machine"
 }
 
 # Exit command menu
@@ -104,23 +129,16 @@ quit() {
 	echo -e "${BLUE}+++++++++++++++++++++++++++++++++++++++++++++++++++++++${NC}"
 	echo -e "${BLUE}    Bye!\n    Au plaisir de vous revoir!${NC}"
 	echo -e "${BLUE}+++++++++++++++++++++++++++++++++++++++++++++++++++++++${NC}"
+	log $USER "********EndScript********"	
 }
 
-#Menu's choices listing
-echo -e "${BLUE}+++++++++++++++++++++++++++++++++++++++++++++++++++++++${NC}"
-echo -e "${BLUE}    Choisir une action ou afficher des informations${NC}"
-echo -e "${BLUE}+++++++++++++++++++++++++++++++++++++++++++++++++++++++${NC}"
-menu1_opt1="Création d'un compte utilisateur"
-menu1_opt2="Suppression d'un compte utilisateur"
-menu1_opt3="Création d'un groupe"
-menu1_opt4="Ajout d'un utilisateur dans un groupe"
-menu1_opt5="Dernière connexion de l'utilisateur"
-menu1_opt6="Nom de la machine"
-menu1_opt7="Nom et Version de l'OS"
-menu1_opt8="Etat du disque"
-menu1_opt9="Redémarrer"
-menu1_opt10="Arrêter"
-menu1_opt11="Quitter"
+# write logs
+log() {
+	line="$(date +%Y%d%m-%H:%M:%S) $1 - $2"
+	echo $line >> /var/log/log_evt.log	
+}
+
+log $USER "********StartScript********"
 
 # Select and options prompt
 PS3="Sélectionner votre choix: "
@@ -140,7 +158,7 @@ do
 		$menu1_opt4)
 			addUserInGroup
 			;;
-		$menu1_opt5)
+		$menu1_opt5)			
 			getUserLastConnection
 			;;
 		$menu1_opt6)
@@ -158,8 +176,8 @@ do
 		$menu1_opt10)
 			shutdownRemoteDevice
 			;;
-		$menu1_opt11)
-			quit
+		$menu1_opt11)					
+			quit			
 			break
 			;;
 		*) echo -e "${RED}Invalid option $REPLY${NC}";;
